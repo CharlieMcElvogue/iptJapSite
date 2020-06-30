34b5166529312a732b71f41a7113e42cf874837a
Charles McElvogue <MCEL01@stpiusx.nsw.edu.au>
2020-07-01 00:51:38 +1000

    Update flashcards.php
    flashcards semi working, need to work on leaving flashcards page then coming back later

--- php/flashcards.php
+++ php/flashcards.php
@@ -1,4 +1,5 @@
 <?php
+//i fixed it :)
 session_start();
 require 'dbUtils.php';
 $conn = getConn();
@@ -7,13 +8,67 @@ if(!isset($_SESSION['userId'])) {
 	header("Location: login.php");
 }
 $userId = $_SESSION['userId'];
-//CARDS STUFF
+
+//CARDS SESSION VARS
+if (!isset($_SESSION['learnSession'])) $_SESSION['learnSession'] = 1;
+if (!isset($_SESSION['counter'])) $_SESSION['counter'] = 0;
+if (!isset($_SESSION['learnArray'])) $_SESSION['learnArray'] = array();
+
+$learnArray = $_SESSION['learnArray'];
+$arrayRef = $_SESSION['counter'];
 
 
+
+//making the learn list
+function makeLearnList()
+{
+	global $userId, $conn;
+	$_SESSION['learnArray'] = array();
+	$learnSession = $_SESSION['learnSession'];
+	$selectSql = "SELECT cardId FROM spacedrepetition WHERE userId = '$userId' AND '$learnSession' % box = 0;";
+	$cardQuery = mysqli_query($conn,$selectSql) or die(mysqli_error($conn));
+	if (mysqli_num_rows($cardQuery)>=1) 
+	{	
+		while($row = mysqli_fetch_assoc($cardQuery)) 
+		{
+			array_push($_SESSION['learnArray'], $row["cardId"]);
+		}
+	}
+}
+
+
+//function that chooses which cards to display:
+
+function displayCard()
+{
+	global $conn;
+
+	if ($_SESSION['counter'] < count($_SESSION['learnArray']))
+	{
+		$cardImg = mysqli_fetch_assoc(mysqli_query($conn, "SELECT cardLoc FROM flashcards WHERE cardId = '".$_SESSION['learnArray'][$_SESSION['counter']]."';"))['cardLoc'];
+		echo "<img src='../img/",$cardImg,"' style='width:20%' ;>";
+
+	}
+	else
+	{
+		$_SESSION['learnSession']++;
+		makeLearnList();
+		
+		$_SESSION['counter'] = 0;
+		$cardImg = mysqli_fetch_assoc(mysqli_query($conn, "SELECT cardLoc FROM flashcards WHERE cardId = '".$_SESSION['learnArray'][$_SESSION['counter']]."';"))['cardLoc'];
+		echo "<img src='../img/",$cardImg,"' style='width:20%' ;>";
+
+	}
+}
+
+print_r($learnArray);
+
+//buttons
 
 //adding new cards
 if (isset($_POST['startCards']))
 {
+	
 	
 	$addSql = "SELECT cardId FROM flashcards WHERE cardId NOT IN (SELECT cardId FROM spacedrepetition WHERE userId = '$userId') LIMIT 5;";
 	$notStarted = mysqli_query($conn,$addSql) or die(mysqli_error($conn));
@@ -28,36 +83,20 @@ if (isset($_POST['startCards']))
 			if (!$stmt->bind_param("ii", $userId, $cardId)) die("BIND FAIL");
 			if (!$stmt->execute()) die($conn->error);
 		}
+	makeLearnList();
+	displayCard();
 	}
 	else {
 		echo "all cards started.";
 	}
 }
-//displaying cards
-$learnSession = 1;
-$learnArray = array();
-$selectSql = "SELECT cardId FROM spacedrepetition WHERE userId = '$userId' AND box/'$learnSession' = 1;";
-$cardQuery = mysqli_query($conn,$selectSql) or die(mysqli_error($conn));
-if (mysqli_num_rows($cardQuery)>=1) {	
-		while($row = mysqli_fetch_assoc($cardQuery)) {
-			array_push($learnArray, $row["cardId"]);
-		}
-}
-print_r($learnArray);
-//buttons
-if (isset($_POST['easy'])){
-	
-}
-if (isset($_POST['normal'])){
-	
-}
-if (isset($_POST['hard'])){
-	
-}
-if (isset($_POST['forgot'])){
-	
-}
 
+
+
+	
+
+echo "<br> array counter is " . $_SESSION['counter'];  //echoes current value of counter
+echo "<br> learn session is " .$_SESSION['learnSession'];
 ?>
 
 
@@ -86,26 +125,32 @@ if (isset($_POST['forgot'])){
 	<main>
 	<form method="POST" action=''>
 	<br>
-		<input type="submit" name="startCards"  value="Start 5 New Cards">
+		<input type="submit" name="startCards"  value="Add 5 new cards">
+	<br>
+		<input type="submit" name="startList"  value="Begin Learning Session">
 	</form> 
-
 <?php
-$arrayRef = 0;
-if ($arrayRef < count($learnArray))
-{
-	$cardImg = mysqli_fetch_assoc(mysqli_query($conn, "SELECT cardLoc FROM flashcards WHERE cardId ='$learnArray[$arrayRef]';"))['cardLoc'];
-	echo "<img src='../img/",$cardImg,"' style='width:20%' ;>";
-	$arrayRef += 1;
+//other buttons
+if (isset($_POST['easy'])) {
+	mysqli_query($conn, "UPDATE spacedrepetition SET box = box+2 WHERE userId = '$userId' AND cardId = '".$_SESSION['learnArray'][$_SESSION['counter']]."';");
+	$_SESSION['counter']++;
+	displayCard();
 }
-else
-{
-	$learnArray = array();
-	$learnSession += 1;
-	$arrayRef = 0;
+if (isset($_POST['normal'])) {
+	mysqli_query($conn, "UPDATE spacedrepetition SET box = box+1 WHERE userId = '$userId' AND cardId = '".$_SESSION['learnArray'][$_SESSION['counter']]."';");
+	$_SESSION['counter']++;
+	displayCard();
+}
+if (isset($_POST['hard'])) {
+	$_SESSION['counter']++;
+	displayCard();
+}
+if (isset($_POST['forgot'])) {
+	mysqli_query($conn, "UPDATE spacedrepetition SET box = 1 WHERE userId = '$userId' AND cardId = '".$_SESSION['learnArray'][$_SESSION['counter']]."';");
+	$_SESSION['counter']++;
+	displayCard();
 }
 ?>
-
-
 
 <div id="showAns" style="text-align:center" >
 The Answer is "a"
@@ -125,6 +170,8 @@ function myFunction() {
   }
 }
 </script>
+
+
 
 
 
